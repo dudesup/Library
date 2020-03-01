ee2a45cca7549f4fa7c31c4d965a3de53d822117
Daria Supińska <dariasupinska@gmail.com>
2020-03-01 13:38:04 +0100

    add book

--- src/main/java/org/sda/java25/library/controller/WelcomeController.java
+++ src/main/java/org/sda/java25/library/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package org.sda.java25.library.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class WelcomeController {
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     public String showWelcomePage(){
-        return "Hello";
+        return "hello";
     }
 }
--- src/main/webapp/jsp/hello.jsp
+++ src/main/webapp/jsp/hello.jsp
@@ -3,7 +3,14 @@
     <title>Elo elo 3 2 0</title>
 </head>
 <body>
+Welcome
 
+<form action="/addBook2" method="post">
+    <input name="title" placeholder="Title">
+    <input name="author" placeholder="Author">
+    <input name="price" placeholder="Price">
+    <input type="submit" value="Add book">
+</form>
 </body>
 
 </html>
